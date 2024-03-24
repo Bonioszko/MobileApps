4b191e24a5f231bcdb3be8f33b571b4d593254c1
Jakub Birecki <kubabir@outlook.com>
2024-03-24 12:33:22 +0100

    Fix: naming

--- app/src/main/java/com/example/szlaki/ListFragment.kt
+++ app/src/main/java/com/example/szlaki/ListFragment.kt
@@ -5,22 +5,21 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ArrayAdapter
 import android.widget.ListView
 import androidx.fragment.app.Fragment
 
 
 /**
  * A simple [Fragment] subclass.
- * Use the [FragmentList.newInstance] factory method to
+ * Use the [ListFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
 
 
-class FragmentList : Fragment() {
+class ListFragment : Fragment() {
     // TODO: Rename and change types of parameters
 
-    lateinit var listView: ListView
+    private lateinit var listView: ListView
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -29,14 +28,14 @@ class FragmentList : Fragment() {
         val view = inflater.inflate(R.layout.fragment_list, container, false)
         listView = view.findViewById(R.id.listview_item)
 
-        val adapter = SzlakAdapter(inflater.context, szlaks)
+        val adapter = TrailAdapter(inflater.context, trails)
         listView.adapter = adapter
         listView.setOnItemClickListener { _, _, position, _ ->
 
-            val szlak = szlaks[position]
-            println(szlak.id)
+            val trail = trails[position]
+            println(trail.id)
             val intent = Intent(inflater.context, DetailActivity::class.java)
-            intent.putExtra("id", szlak.id)
+            intent.putExtra("id", trail.id)
             startActivity(intent)
         }
         return view
